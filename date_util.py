33137bab4e1f6a2e1a6abe355c89bab2a8a6a4fc
achen1296 <achen1296@gmail.com>
2026-01-01 22:13:06 -0500

    remove utc time zone

--- date_util.py
+++ date_util.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 
 def datetime_to_int(d: datetime | None = None):
     # +2082844800 to convert Unix epoch (Jan 1 1970) to Mac epoch (Jan 1 1904)
     if d is None:
-        d = datetime.now(timezone.utc)
+        d = datetime.now()
     return int(d.timestamp()) + 2082844800
 
 
 def int_to_datetime(i: int):
     i -= 2082844800
-    return datetime.fromtimestamp(i, timezone.utc)
+    return datetime.fromtimestamp(i)
