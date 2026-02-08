abb83580ada9667ecd654276809392e3bcc4481f
achen1296 <achen1296@gmail.com>
2026-02-08 17:28:37 -0500

    stub for custom protocol to serve music files to the frontend

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { app, BrowserWindow } from 'electron/main';
+import { net } from 'electron';
+import { app, BrowserWindow, protocol } from 'electron/main';
+import { pathToFileURL } from 'node:url';
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -9,8 +11,26 @@ const createWindow = () => {
     win.loadFile('index.html');
 };
 
+protocol.registerSchemesAsPrivileged([
+    { scheme: "app", privileges: { stream: true, bypassCSP: true } }
+]);
+
 app.whenReady().then(() => {
     createWindow();
+
+    protocol.handle("app", (req) => {
+        const { host, pathname } = new URL(req.url);
+
+        if (host === "track") {
+            // todo for testing
+            return net.fetch(pathToFileURL("test.mp3").toString());
+        }
+
+        return new Response("unknown host", {
+            status: 400,
+            headers: { 'content-type': 'text/html' }
+        });
+    });
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
