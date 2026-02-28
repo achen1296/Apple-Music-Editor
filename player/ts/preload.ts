f721b3b49a625fe7f7527784ebb63305e05eafe5
achen1296 <achen1296@gmail.com>
2026-02-28 17:27:04 -0500

    removed player -- put it in a new repo which will use this one as a submodule instead

--- player/ts/preload.ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import { ipcRenderer } from "electron";
-import { contextBridge } from "electron/renderer";
-
-contextBridge.exposeInMainWorld(
-    "backendRequest", (url: string) => ipcRenderer.invoke("backendRequest", url)
-);
